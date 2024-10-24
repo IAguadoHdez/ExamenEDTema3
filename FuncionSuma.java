9bf37ccec7ee365df446133c0017a2ffbe0047f4
IAguadoHdez <iaguado20938@iesjoanramis.org>
2024-10-24 17:50:10 +0200

    Comentarios javadoc añadidos

--- FuncionSuma.java
+++ FuncionSuma.java
@@ -1,16 +1,33 @@
 package masejercicios;
 import java.util.Scanner;
-
+/**
+*<p>Programa suma,para calcular la suma de dos valores introducidos</p>
+*@version v1.0
+*@author Miquel
+*@see Para más informacion ver en <a href="https://www.google.com>Google</a>
+*/
 
 
 public class FuncionSuma {
 
-	
-	
+	/**
+	*
+	*Variable para guardar el primer sumando
+	*
+	*/
 	private static int primerSumando;
 	
+	/**
+	*
+	*Variable para guardar el segundo sumando
+	*/
 	private static int segundoSumando; 
-	
+
+	/**
+	*
+	*Funcio principal del programa
+	*@param args El parametro habitual de la funcion main
+	*/
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
@@ -28,7 +45,14 @@ public class FuncionSuma {
 	}
 	
 			
-	
+	/**
+	*
+	*Funcion que realiza la suma de los dos operadores
+	*@param
+	* sumando 1 - primer sumando de la operacion suma
+	* sumando 2 - segundo sumando de la operacion suma
+	*@return Devuelve el resultado de sumar los dos sumandos
+	*/
 	private static int suma (int sumando1, int sumando2) {
 		return sumando1-sumando2;
 	}
